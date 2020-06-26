7cefbf05730fc59fa5ce24f9b1c56898999c84ef
xiaolanglang <2145773+xiaolanglanglang@users.noreply.github.com>
2020-06-27 00:59:12 +0800

    feat: Use task to blink the LED.
    Signed-off-by: xiaolanglang <2145773+xiaolanglanglang@users.noreply.github.com>

--- apps/app/src/lib.rs
+++ apps/app/src/lib.rs
@@ -1,24 +1,39 @@
 #![no_std]
 
+#[allow(dead_code)]
+#[allow(non_camel_case_types)]
+#[allow(non_upper_case_globals)]
+mod mynewt;
+
 mod wrapper;
+
+use wrapper::*;
 
 extern crate panic_halt;
 
-
 #[no_mangle]
 pub extern "C" fn main() -> ! {
-    /* Initialize all packages. */
-    wrapper::sys_init_start();
-    wrapper::sys_init_app();
-    wrapper::sys_init_end();
+    sys_init_start();
 
-    wrapper::gpio_init_out(wrapper::LED_BLINK_PIN, 1);
+    sys_init_app();
+
+    let mut blinky = wrapper::os_task_build();
+    let blinky_stack = [0; 32];
+    os_task_init(&mut blinky, b"blinky", blinky_fn, 10, &blinky_stack);
+
+    sys_init_end();
+
+    main_loop();
+}
+
+pub extern "C" fn blinky_fn(_arg: *mut ::cty::c_void) {
+    gpio_init_out(LED_BLINK_PIN, 1);
 
     loop {
         /* Wait one second */
-        wrapper::os_delay_ms(1000);
+        os_delay_ms(1000);
 
         /* Toggle the LED */
-        wrapper::gpio_toggle(wrapper::LED_BLINK_PIN);
+        gpio_toggle(LED_BLINK_PIN);
     }
 }
--- apps/app/src/wrapper.rs
+++ apps/app/src/wrapper.rs
@@ -1,3 +1,8 @@
+use crate::mynewt::os;
+use crate::mynewt::os::{os_task, os_stack_t};
+use cty::c_char;
+use core::ffi::c_void;
+
 pub const LED_BLINK_PIN: i32 = 45;
 
 extern "C" {
@@ -31,4 +36,41 @@ pub fn gpio_init_out(pin: i32, val: i32) -> i32 {
 
 pub fn os_delay_ms(ms: u32) {
     unsafe { os_time_delay(ms) }
+}
+
+pub fn os_task_build() -> os::os_task {
+    unsafe {
+        core::mem::transmute::<[u8; core::mem::size_of::<os::os_task>()], os_task>(
+            [0; core::mem::size_of::<os::os_task>()]
+        )
+    }
+}
+
+pub fn os_task_init(
+    task: &mut os::os_task,
+    task_name: &[u8],
+    task_fn: extern "C" fn(*mut c_void),
+    priority: u8,
+    task_stack: &[os::os_stack_t],
+) {
+    unsafe {
+        os::os_task_init(
+            task,                                   //  Task object will be saved here
+            task_name.as_ptr() as *const c_char,    //  Name of task
+            Some(task_fn),                          //  Function to execute when task starts
+            0 as *mut c_void,                       //  Argument to be passed to above function
+            priority,                               //  Task priority: highest is 0, lowest is 255 (main task is 127)
+            os::OS_WAIT_FOREVER as u32,             //  Don't do sanity / watchdog checking
+            task_stack.as_ptr() as *mut os_stack_t, //  Stack space for the task
+            task_stack.len() as u16,                //  Size of the stack (in 4-byte units)
+        );
+    }
+}
+
+pub fn main_loop() -> ! {
+    loop {
+        unsafe {
+            os::os_eventq_run(os::os_eventq_dflt_get())
+        }
+    }
 }
